c4f5bbb9f7818fb9874d2251887fb571a289c1fb
Romain Beaumont <romain.rom1@gmail.com>
2015-08-22 20:45:00 +0200

    change the pypi module name for coherence

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='minecraft-data',
-    description='Provide easy access to minecraft-data in python',
+    name='minecraft_data',
+    description='Provide easy access to minecraft data in python',
     license='MIT',
     long_description=open('README.rst').read(),
     version='0.0.1',
