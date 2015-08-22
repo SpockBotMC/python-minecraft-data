05350d216c523e7ec4e2c618590ed0c320affab2
Romain Beaumont <romain.rom1@gmail.com>
2015-08-22 21:18:53 +0200

    simple raw data mono version support

--- minecraft_data/__init__.py
+++ minecraft_data/__init__.py
@@ -1,4 +1,15 @@
 import json
 
-f = open('data/1.8/enums/recipes.json', 'r')
-recipes = json.loads(f.read())
+
+def read_json(name, version):
+    f = open('data/'+version+'/enums/'+name+'.json', 'r')
+    return json.loads(f.read())
+
+blocks = read_json('blocks', '1.8')
+biomes = read_json('biomes', '1.8')
+items = read_json('items', '1.8')
+recipes = read_json('recipes', '1.8')
+instruments = read_json('instruments', '1.8')
+materials = read_json('materials', '1.8')
+entities = read_json('entities', '1.8')
+protocol = read_json('protocol', '1.8')
