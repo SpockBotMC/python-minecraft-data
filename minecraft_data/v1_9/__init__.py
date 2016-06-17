0f61e240a04de56aaef5e6a44c96d527a9b4f242
Gjum <code.gjum@gmail.com>
2016-06-17 23:07:21 +0200

    update to minecraft-data 2.2.0

--- minecraft_data/v1_9/__init__.py
+++ minecraft_data/v1_9/__init__.py
@@ -4,7 +4,7 @@ import sys
 from minecraft_data.tools import convert
 
 
-_dir = os.path.join(os.path.dirname(__file__),"../data/data/1.9")
+_dir = os.path.join(os.path.dirname(__file__), "../data/data/pc/1.9")
 
 for name, data in convert(_dir).items():
     setattr(sys.modules[__name__], name, data)
