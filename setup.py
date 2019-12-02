755705c9ccdf239e32a499e427ab238356a369f6
Alexander Bean Apmann <alex.apmann@gmail.com>
2019-12-01 20:48:03 -0500

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
+import os
+
 from distutils.core import setup
 
 from setuptools import find_packages
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('minecraft_data/data/data')
 
 setup(
     name='minecraft_data',
@@ -12,7 +23,7 @@ setup(
     maintainer_email='romain.rom1@gmail.com',
     url='https://github.com/SpockBotMC/python-minecraft-data',
     packages=find_packages(),
-    package_data={'minecraft_data': ["data/data/*/*.json"]},
+    package_data={'minecraft_data': extra_files},
     install_requires=[
     ],
     keywords=['minecraft'],
