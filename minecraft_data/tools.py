cfc9e4ff9be08b96d2aebaece7606bdbdd632ad9
Vito Gamberini <vito@gamberini.email>
2020-07-30 12:43:52 -0400

    Add missing MCD-provided data, improve tools.py
    Added all the missing data from MCD, specifically:
    * blockCollisionShapes
    * enchantments
    * foods
    * protocolComments
    
    Converting data is very simple now, adding new json files is as simple
    as adding their name to a list.
    Non-breaking change to data access:
    * instruments and biomes now provide a _name version to access their
      ids by name, same as blocks, items, windows, etc

--- minecraft_data/tools.py
+++ minecraft_data/tools.py
@@ -4,50 +4,26 @@ from glob import glob
 
 
 def convert(_dir, version, edition ='pc'):
-    fp = open(os.path.join(_dir, 'dataPaths.json'))
-    datapaths = json.load(fp)
-    fp.close()
+    with open(os.path.join(_dir, 'dataPaths.json')) as f:
+        datapaths = json.load(f)
     data = _grabdata(_dir, datapaths[edition][version])
     ret = {}
-    if 'blocks' in data:
-        ret['blocks'] = _by_id(data['blocks'])
-        ret['blocks_name'] = _by_name(data['blocks'])
-        ret['blocks_list'] = data['blocks']
-    if 'items' in data:
-        ret['items'] = _by_id(data['items'])
-        ret['items_name'] = _by_name(data['items'])
-        ret['items_list'] = data['items']
-    if 'biomes' in data:
-        ret['biomes'] = _by_id(data['biomes'])
-        ret['biomes_list'] = data['biomes']
-    if 'recipes' in data:
-        ret['recipes'] = data['recipes']
-    if 'instruments' in data:
-        ret['instruments'] = _by_id(data['instruments'])
-        ret['instruments_list'] = data['instruments']
-    if 'materials' in data:
-        ret['materials'] = data['materials']
+    for datum in ('recipes', 'materials', 'protocol', 'version',
+            'blockCollisionShapes', 'protocolComments'):
+        if datum in data:
+            ret[datum] = data[datum]
+    for datum in ('blocks', 'items', 'windows', 'effects', 'particles',
+            'biomes', 'instruments', 'enchantments', 'foods'):
+        if datum in data:
+            ret[datum] = _by_id(data[datum])
+            ret[f"{datum}_name"] = _by_name(data[datum])
+            ret[f"{datum}_list"] = data[datum]
     if 'entities' in data:
         ret['mobs'] = _by_id(_filter('type', 'mob', data['entities']))
         ret['objects'] = _by_id(_filter('type', 'object', data['entities']))
         ret['entities_name'] = _by_name(data['entities'])
         ret['entities_list'] = data['entities']
-    if 'protocol' in data:
-        ret['protocol'] = data['protocol']
-    if 'windows' in data:
-        ret['windows'] = _by_id(data['windows'])
-        ret['windows_name'] = _by_name(data['windows'])
-        ret['windows_list'] = data['windows']
-    if 'effects' in data:
-        ret['effects'] = _by_id(data['effects'])
-        ret['effects_name'] = _by_name(data['effects'])
-        ret['effects_list'] = data['effects']
-    if 'version' in data:
-        ret['version'] = data['version']
-    if 'particles' in data:
-        ret['particles'] = _by_id(data['particles'])
-        ret['particles_name'] = _by_name(data['particles'])
-        ret['particles_list'] = data['particles']
+
 
     def find_item_or_block(find):
         if isinstance(find, int):  # by id
