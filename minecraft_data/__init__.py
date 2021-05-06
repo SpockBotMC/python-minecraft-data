a7f1cf854c84da31741f1bdd5986ac731774f231
sqozz <sqozz@geekify.de>
2021-05-06 20:29:17 +0200

    Add common data API endpoint
    This fixes #20

--- minecraft_data/__init__.py
+++ minecraft_data/__init__.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from minecraft_data.tools import convert
+from minecraft_data.tools import convert, commondata
 
 
 class mod(sys.modules[__name__].__class__):
@@ -10,6 +10,12 @@ class mod(sys.modules[__name__].__class__):
             os.path.dirname(__file__), "data/data/"
         )
         return type(version, (object,), convert(_dir, version, edition))
+
+    def common(self, edition = 'pc'):
+        _dir = os.path.join(
+            os.path.dirname(__file__), "data/data/"
+        )
+        return type('common', (object,), commondata(_dir, edition))
 
 
 sys.modules[__name__].__class__ = mod
