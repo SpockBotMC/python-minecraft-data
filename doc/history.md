6b816dd4c714fa49f5fc341d2a88f0fa893ecc1f
Vito Gamberini <vito@gamberini.email>
2021-06-16 18:02:17 -0400

    Release 2.85.1

--- doc/history.md
+++ doc/history.md
@@ -1,3 +1,6 @@
+## 2.85.1
+ * Update minecraft-data to 2.85.1
+
 ## 2.84.0
  * Update minecraft-data to 2.84.0
  * Add "common" data (thanks sqozz)
--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+
 def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
         for filename in filenames:
             paths.append(os.path.join('..', path, filename))
     return paths
+
 
 extra_files = package_files('minecraft_data/data/data')
 
@@ -18,16 +20,15 @@ setup(
     description='Provide easy access to minecraft data in python',
     license='MIT',
     long_description=open('README.rst').read(),
-    version='2.84.0',
-    maintainer='Romain Beaumont',
-    maintainer_email='romain.rom1@gmail.com',
+    version='2.85.1',
+    maintainer='Vito Gamberini',
+    maintainer_email='vito@gamberini.email',
     url='https://github.com/SpockBotMC/python-minecraft-data',
     packages=find_packages(),
     package_data={'minecraft_data': extra_files},
-    install_requires=[
-    ],
+    install_requires=[],
     keywords=['minecraft'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
-    ]
+    ],
 )
