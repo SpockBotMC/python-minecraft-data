c02e798dedde2595adae88c228b5f804b0214fc3
Joachim Coenen <29330338+JoachimCoenen@users.noreply.github.com>
2021-12-26 23:55:56 +0100

    Release 2.100.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description='Provide easy access to minecraft data in python',
     license='MIT',
     long_description=open('README.rst').read(),
-    version='2.89.2',
+    version='2.100.0',
     maintainer='Vito Gamberini',
     maintainer_email='vito@gamberini.email',
     url='https://github.com/SpockBotMC/python-minecraft-data',
